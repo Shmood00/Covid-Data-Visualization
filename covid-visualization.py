5dae87e27e52470e2f6f70f6ab0bf9baeb18a4ef
Shmood00 <43577426+Shmood00@users.noreply.github.com>
2020-11-29 20:31:15 -0500

    Update covid-visualization.py

--- covid-visualization.py
+++ covid-visualization.py
@@ -15,7 +15,7 @@ from flask_caching import Cache
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = "COVID-19 Visualization In Canada"
+app.title = "COVID-19 Visualization"
 app.config.suppress_callback_exceptions = True
 
 #Configure Flask-Caching
